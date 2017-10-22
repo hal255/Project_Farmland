fd801a5661882052f173a781347c6a572e8b969d
Hong Luu <hluu114@gmail.com>
2017-10-22 00:25:53 -0700

    Started gravity C# script * Added scripts folder * Applied gravity.cs to player character

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+    private float gravity = -9.8f;
+    private bool is_grounded = false;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!is_grounded)
+        {
+            transform.Translate(Vector3.up * gravity * Time.deltaTime);
+        }
+    }
+
+}
