06338015921db0fc340dfa7a588fb15747342e81
Hong Luu <hluu114@gmail.com>
2017-10-22 22:55:42 -0700

    Adjusted gravity functionality * Decided to let Unity built in gravity do the task for player capsule * Removed gravity.cs from player * Added rigidbody to player capsule, checked use gravity * Unchecked is_trigger from floor to allow collision

--- Assets/Scripts/Player_scripts/Player_Motion.cs
+++ Assets/Scripts/Player_scripts/Player_Motion.cs
@@ -21,21 +21,40 @@ public class Player_Motion : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         // print debug messages
-        debug_logs.print_messages();
+        //debug_logs.print_messages();
 
         // check if player is collided with ground
+        //if (gameObject.GetComponent<Collider>().tag == "floor")
+        //{
+        //    handle_floor_collision(gameObject.GetComponent<Collider>());
+        //}
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        debug_logs.add_message("player_vertical_collision", collision.gameObject.name);
+        handle_floor_collision(collision.gameObject.name);
+
+        //// Debug-draw all contact points and normals
+        //foreach (ContactPoint contact in collision.contacts)
+        //{
+        //    Debug.DrawRay(contact.point, contact.normal, Color.white);
+        //}
+
+        //// Play a sound if the colliding objects had a big impact.
+        //if (collision.relativeVelocity.magnitude > 2)
+        //    audioSource.Play();
+    }
     void set_player_gravity(float speed)
     {
-        player_gravity.set_gravity(-1.0f);
+        player_gravity.set_gravity(speed);
         debug_logs.add_message("player_gravity", speed + "");
     }
 
-    void handle_floor_collision(Collider other)
+    void handle_floor_collision(string object_name)
     {
         set_player_gravity(0.0f);
-        debug_logs.add_message("player_vertical_collision", other.gameObject.name);
+        debug_logs.add_message("player_vertical_collision", object_name);
     }
 
 }
