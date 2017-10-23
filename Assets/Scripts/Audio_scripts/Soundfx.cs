ca03073697c5bb20d0ad37715353ed678f077e9e
Hong Luu <hluu114@gmail.com>
2017-10-22 23:03:53 -0700

    Started on Audio * Added Soundfx.cs * Added BGM.cs

---
+++ Assets/Scripts/Audio_scripts/Soundfx.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundfx : MonoBehaviour {
+    AudioSource audio_source;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // plays sound on demand
+    void play_sound()
+    {
+        if (audio_source == null)
+        {
+            gameObject.GetComponent<My_Debug_Logs>().add_message(gameObject.name + " soundfx failed", "No source");
+            return;
+        }
+
+        if (!audio_source.isPlaying)
+        {
+            gameObject.GetComponent<My_Debug_Logs>().add_message(gameObject.name + " soundfx", "is playing " + audio_source.name);
+            audio_source.Play();
+        }
+    }
+
+
+}
