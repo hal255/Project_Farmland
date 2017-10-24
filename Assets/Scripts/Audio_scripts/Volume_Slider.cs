d87afb953dd8182894d0a127d683394fde7d1e02
Hong Luu <hluu114@gmail.com>
2017-10-24 00:27:39 -0700

    Added Volume Control * Audio Listener on main camera * Audio Source on player_01 * Added UI for volume scrollbar (slider) * Volume_slider.cs ** Real time updates on volume of audio listener based on user feedback on slider

---
+++ Assets/Scripts/Audio_scripts/Volume_Slider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume_Slider : MonoBehaviour {
+    //[SerializeField] private AudioListener player_listener;
+    private Scrollbar volume_slider;
+    private Text volume_label;
+    private float volume;
+
+	// Use this for initialization
+	void Start () {
+        volume_slider = gameObject.GetComponent<Scrollbar>();
+        volume_label = gameObject.GetComponentInChildren<Text>();
+        volume = volume_slider.value * 100;
+        volume_label.text = "Volume " + (int)volume + "%";
+        Debug.Log(volume_label.text);
+
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        volume_slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        AudioListener.volume = volume_slider.value;
+        volume = volume_slider.value * 100;
+        volume_label.text = "Volume " + (int)volume + "%";
+        Debug.Log(volume_label.text);
+        Debug.Log(volume_slider.value);
+    }
+}
