d84a0f1b808b0a5ffcc5f12dff0bb104c9c2acb2
Hong Luu <hluu114@gmail.com>
2017-10-28 01:53:03 -0700

    Modified audio controller

--- Assets/Scripts/Player_scripts/Player_Audio_Controller.cs
+++ Assets/Scripts/Player_scripts/Player_Audio_Controller.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Audio_Controller : MonoBehaviour {
-    Audio_Controller player_audio;
+    private List<AudioClip> audio_clips;
 
 	// Use this for initialization
 	void Start () {
-        player_audio = gameObject.GetComponent<Audio_Controller>();
-        AudioSource new_source = new AudioSource();
-        new_source.clip = Resources.Load<AudioClip>("Audio/berserk_forces");
-        //new_source.clip = Resources.Load("Audio/berserk_forces.mp3") as AudioClip;
-        Debug.Log("Added Clip berserk_force as battle");
-        player_audio.AddAudioSource("battle", new_source);
-        player_audio.PlayAudio("battle");
+        audio_clips = new List<AudioClip>();
+        AudioClip clip = Resources.Load("Audio/berserk_forces") as AudioClip;
+        clip.name = "battle";
+        audio_clips.Add(clip);
+        foreach(AudioClip audio_clip in audio_clips)
+        {
+            if(audio_clip.name.Equals("battle"))
+                AudioSource.PlayClipAtPoint(clip, gameObject.transform.position, 1.0f);
+        }
 	}
 }
