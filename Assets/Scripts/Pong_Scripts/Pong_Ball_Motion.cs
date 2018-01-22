af35bf48d8718d07f2f5d8e5bdf429d56c94404b
Hong Luu <hluu114@gmail.com>
2018-01-21 19:28:24 -0800

    feature/pong_game * fixed issue with players colliding with walls * Issue resolved using sentinel values preventing player movements

--- Assets/Scripts/Pong_Scripts/Pong_Ball_Motion.cs
+++ Assets/Scripts/Pong_Scripts/Pong_Ball_Motion.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pong_Ball_Motion : MonoBehaviour {
+    private bool debug = true;
+
     [SerializeField]
     float move_speed = 10.0f;               // velocity of ball
     private float rotate_angle = 0.0f;      // angle of ball, from top down view
 
-    float added_speed = 0.0f;               // added speed from user's paddle
+    //float added_speed = 0.0f;               // added speed from user's paddle
 
 	// Use this for initialization
 	void Start () {
         // generate a random angle and move towards that direction
-        float new_angle = Random.value * 359;
-        transform.Rotate(Vector3.up * new_angle);       // "up" is the y-axis, the top-down view
+        rotate_angle = GetNewAngle(0);
+        SetBallRotation();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         transform.Translate(Vector3.forward * move_speed * Time.deltaTime);
 	}
+
+    float GetNewAngle(float angle)
+    {
+        angle = Random.value * 359;
+        if ((angle >= 25f && angle <= 155f) || (angle >= 205f && angle <= 335f))
+            return angle;
+        else
+            return GetNewAngle(angle);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (debug)
+            Debug.Log("Colision detected!!!!!!!! Pong ball current angle: " + rotate_angle);
+        rotate_angle = (rotate_angle > 180f) ? rotate_angle - 180f : rotate_angle + 180f;
+        SetBallRotation();
+    }
+
+    void SetBallRotation()
+    {
+        transform.Rotate(Vector3.up * rotate_angle);       // "up" is the y-axis, the top-down view
+        if (debug)
+            Debug.Log("Pong ball new angle: " + rotate_angle);
+    }
 }
--- Assets/Scripts/Pong_Scripts/Pong_Player_Keyboard_Controls.cs
+++ Assets/Scripts/Pong_Scripts/Pong_Player_Keyboard_Controls.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pong_Player_Keyboard_Controls : MonoBehaviour {
+    private bool debug = false;
 
     [SerializeField]
     float move_speed = 1.0f;
 
     private float modifier = 1.0f;
     private bool isPlayer01 = true;
+    private bool isWestWallCollided = false;        // if colliding with wall, stop moving in direction of west wall
+    private bool isEastWallCollided = false;        // if colliding with wall, stop moving in direction of east wall
 
     // Use this for initialization
     void Start () {
-        if (gameObject.name.Contains("02"))
+        if (gameObject.name.Contains("2"))
         {
             modifier *= -1;
             isPlayer01 = false;
@@ -24,25 +27,29 @@ public class Pong_Player_Keyboard_Controls : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.A) && isPlayer01)
         {
-            Debug.Log("Pressed player1 left key.");
+            if (debug)
+                Debug.Log("Pressed player1 left key.");
             moveLeft();
         }
 
         if (Input.GetKey(KeyCode.D) && isPlayer01)
         {
-            Debug.Log("Pressed player1 right key.");
+            if (debug)
+                Debug.Log("Pressed player1 right key.");
             moveRight();
         }
 
         if (Input.GetKey(KeyCode.J) && !isPlayer01)
         {
-            Debug.Log("Pressed player2 left key.");
+            if (debug)
+                Debug.Log("Pressed player2 left key.");
             moveLeft();
         }
 
         if (Input.GetKey(KeyCode.L) && !isPlayer01)
         {
-            Debug.Log("Pressed player2 right key.");
+            if (debug)
+                Debug.Log("Pressed player2 right key.");
             moveRight();
         }
 
@@ -50,11 +57,44 @@ public class Pong_Player_Keyboard_Controls : MonoBehaviour {
 
     private void moveLeft()
     {
-        transform.Translate(Vector3.forward * move_speed * -1.0f * modifier * Time.deltaTime);
+        // if colliding with west wall, stop moving west
+        if (!isWestWallCollided)
+        {
+            if (move_speed > 0)
+                move_speed *= -1.0f;
+            transform.Translate(Vector3.forward * move_speed * modifier * Time.deltaTime);
+        }
     }
     private void moveRight()
     {
-        transform.Translate(Vector3.forward * move_speed * modifier * Time.deltaTime);
+        // if colliding with east wall, stop moving east
+        if (!isEastWallCollided)
+        {
+            if (move_speed < 0)
+                move_speed *= -1.0f;
+            transform.Translate(Vector3.forward * move_speed * modifier * Time.deltaTime);
+        }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        string collided_name = collision.gameObject.name;
+        Debug.Log(gameObject.name + " collided with " + collided_name);
+
+        if (collided_name.Contains("west"))
+            isWestWallCollided = true;
+        if (collided_name.Contains("east"))
+            isEastWallCollided = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        string collided_name = collision.gameObject.name;
+        Debug.Log(gameObject.name + " no longer collides with " + collided_name);
+
+        if (collided_name.Contains("west"))
+            isWestWallCollided = false;
+        if (collided_name.Contains("east"))
+            isEastWallCollided = false;
+    }
 }
